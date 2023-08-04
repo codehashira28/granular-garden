7adac45f1b7aad3e3a1a1ef4b77721e523a846ee
Cameron French <cameron.french92@gmail.com>
2023-08-03 21:15:40 -0400

    Setup a router for profile to show song data particular to that user - added auth.js file

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const { User, Feed } = require('../models');
+const withAuth = require('../utils/auth');
+
 
 router.get('/', async (req, res) => {
     res.render('homepage', { pageTitle: 'Home' });
@@ -27,5 +29,28 @@ router.get('/feed', async (req, res) => {
       res.status(500).json(err);
     }
   });
+
+  // route to get profile that shows just the users songs
+  router.get('/profile', withAuth, async (req, res) => {
+    try {
+        const dbSongData = await dbSongData.findAll({
+            where: {
+                creator: req.session.userId,
+            },
+            order: [
+                ['id', 'DESC'],
+            ],
+        });
+        
+        const songdata = serialize(dbSongData);
+        res.render('profile', {
+            songdata: songdata,
+            loggedIn: req.session.loggedIn,
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
