9f0260994e564b765f4f8aba540ba52186369f50
zoe <zoeedge0703@gmail.com>
2023-08-07 22:10:13 -0500

    added the comment section

--- controllers/api/feed-routes.js
+++ controllers/api/feed-routes.js
@@ -4,11 +4,51 @@ const { Feed, Comment } = require('../../models');
 // Get all songs for feed
 router.get('/', async (req, res) => {
     // have this include: Comments.
-    const feedData = await Feed.findAll({
-        include: [Comment]
+    try {const feedData = await Feed.findAll({
+        include: [{model: Comment, as: 'comment'}]
+    });
+    res.status(200).json(feedData);
+} catch (error) {
+    console.error('Error fetching feed data:', error);
+    res.status(500).json({ error: 'Error fetching feed data' })
+};
+
 });
 
-    res.status(200).json(feedData);
+router.get('/track/:feed_id', async (req,res) => {
+    try {
+        const feedId = req.params.feed_id
+        const feedData = await Feed.findByPk(feedId, {
+            include: [{model: Comment, as: 'comment'}]
+        });
+
+        res.render('track-details', { feedData })
+
+    } catch (error) {
+        console.error('Error fetching track details:', error);
+        res.status(500).json({ error: 'Error fetching track details' })
+    }
+})
+
+router.post('/comment/add/:feed_id', async (req,res) => {
+    try {
+        const { feedId } = req.params
+        const { commentText } = req.body
+        const newComment = await Comment.create({
+            text: commentText,
+            feed_id: feedId
+        });
+
+        const updatedFeedData = await Feed.findByPk(feedId, {
+            include: [{model: Comment, as: 'comment'}]
+        }); 
+
+        res.status(201).json(updatedFeedData);
+
+    } catch (error) {
+        console.error('Error adding comment:', error);
+        res.status(500).json({ error: 'Error adding comment' })
+    };
 })
 
 
